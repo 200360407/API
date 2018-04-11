4972de0f6657ea67bf8cac2198dbf8378af25fb0
200360407 <200360407.student.georgianc.on.ca>
2018-04-11 17:28:23 -0400

    first commit

---
+++ API/Models/child.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("child")]
+    public class child
+    {
+
+        [Key]
+        [Required]
+        public int color2 { get; set; }
+
+       [Required]
+        public int ID { get; set; }
+        [Required]
+        public string location { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
